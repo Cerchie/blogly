10b21a6fccf20df62309612c72ef26b2ad70ddc8
Lucia Cerchie <luciacerchie@gmail.com>
2020-08-04 16:19:52 -0700

    taking user class out of app.py

--- app.py
+++ app.py
@@ -34,17 +34,16 @@ def list_users():
 
 @app.route("/users/new", methods=['POST', 'GET'])
 def add_user():
-
-    new_user = User(
-        first_name=request.form['first_name'],
-        last_name=request.form['last_name'],
-        image_url=request.form['image_url'])
-    users = User.query.all()
+    """add user and redirect to list"""
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
 
     user = User(first_name=first_name,
                 last_name=last_name, image_url=image_url)
     db.session.add(user)
     db.session.commit()
+
     return redirect(f"/users")
 
 
